85ce4136c8ba29d5c1a0f57ddd07bc734c3a7814
SJ Kim <sjkim0421@gmail.com>
2020-02-11 11:18:03 -0500

    Modify player stats scraper to scrape stats per game, per 36 minutes and totals

--- webscraper.py
+++ webscraper.py
@@ -5,33 +5,41 @@ import re
 import pdb;
 
 def scrape_players_data():
-    url = 'https://www.basketball-reference.com/leagues/NBA_2020_per_game.html'
-    response = requests.get(url, timeout=5)
-    content = BeautifulSoup(response.content, "html.parser")
-    playerRows = content.findAll('tr', attrs={"class": "full_table"})
-    dataStats = content.findAll('td', {"data-stat": re.compile(r".*")})
+    fileNameToUrlMap = {
+        'statsPerGame': 'https://www.basketball-reference.com/leagues/NBA_2020_per_game.html',
+        'statsPer36': 'https://www.basketball-reference.com/leagues/NBA_2020_per_minute.html',
+        'statsTotal': 'https://www.basketball-reference.com/leagues/NBA_2020_totals.html'
+    }
 
-    # get all data-stat attributes
-    categories = set()
-    for dataStat in dataStats:
-        categories.add(dataStat.get("data-stat"))
 
-    # create playerObject JSONs
-    playersArr = []
-    for playerRow in content.findAll('tr', attrs={"class": "full_table"}):
-        playerObject = {}
-        for category in categories:
-            categoryColumn = playerRow.find('td', attrs={"data-stat": category})
-            if category == 'player':
-                playerObject["first_name"] = categoryColumn.get("csk").split(",")[1]
-                playerObject["last_name"] = categoryColumn.get("csk").split(",")[0]
-            else:
-                playerObject[category] = categoryColumn.text
+    for fileName in fileNameToUrlMap:
+        url = fileNameToUrlMap[fileName]
+        response = requests.get(url, timeout=5)
+        content = BeautifulSoup(response.content, "html.parser")
+        playerRows = content.findAll('tr', attrs={"class": "full_table"})
+        dataStats = content.findAll('td', {"data-stat": re.compile(r".*")})
 
-        playersArr.append(playerObject)
+        # get all data-stat attributes
+        categories = set()
+        for dataStat in dataStats:
+            categories.add(dataStat.get("data-stat"))
 
-    with open('json_data/playersData.json', 'w') as outfile:
-        json.dump(playersArr, outfile)
+        # create playerObject JSONs
+        playersArr = []
+        for playerRow in content.findAll('tr', attrs={"class": "full_table"}):
+            playerObject = {}
+            for category in categories:
+                categoryColumn = playerRow.find('td', attrs={"data-stat": category})
+                if category == 'player':
+                    playerObject["first_name"] = categoryColumn.get("csk").split(",")[1]
+                    playerObject["last_name"] = categoryColumn.get("csk").split(",")[0]
+                else:
+                    playerObject[category] = categoryColumn.text
+
+            playersArr.append(playerObject)
+
+        with open(f'json_data/{fileName}.json', 'w') as outfile:
+            json.dump(playersArr, outfile)
 
 
 def scrape_schedule_data():
